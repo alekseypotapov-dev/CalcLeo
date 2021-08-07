1e4a047e4e2c3e939be4034f049dddff0e03bde3
Aleksey Potapov <ing.aleksey.potapov@gmail.com>
2021-08-07 20:53:14 +0200

    Logic: code clear

--- CalcLeo/CalcLeo/Classes/Views/CalculatorView.swift
+++ CalcLeo/CalcLeo/Classes/Views/CalculatorView.swift
@@ -55,60 +55,6 @@ final class CalculatorView: UIView, CalculatorViewModelDelegate {
         ])
     }
 
-    func zzz() {
-        var button = [UIButton]()
-
-        for i in 0..<20 {
-            let btn = UIButton(type: .custom)
-            btn.setTitle("\(i)", for: .normal)
-            btn.backgroundColor = .cyan
-            btn.translatesAutoresizingMaskIntoConstraints = false
-            btn.tag = i
-            button.append(btn)
-        }
-
-        let firstColumnVerticalStackView = UIStackView(arrangedSubviews: [button[0], button[1], button[2], button[3]])
-        firstColumnVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        firstColumnVerticalStackView.axis = .vertical
-        firstColumnVerticalStackView.alignment = .fill
-        firstColumnVerticalStackView.distribution = .fillEqually
-        firstColumnVerticalStackView.spacing = 5
-
-        let secondColumnVerticalStackView = UIStackView(arrangedSubviews: [button[4], button[5], button[6], button[7]])
-        secondColumnVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        secondColumnVerticalStackView.axis = .vertical
-        secondColumnVerticalStackView.alignment = .fill
-        secondColumnVerticalStackView.distribution = .fillEqually
-        secondColumnVerticalStackView.spacing = 5
-
-        let thirdColumnVerticalStackView = UIStackView(arrangedSubviews: [button[8], button[9], button[10], button[11]])
-        thirdColumnVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        thirdColumnVerticalStackView.axis = .vertical
-        thirdColumnVerticalStackView.alignment = .fill
-        thirdColumnVerticalStackView.distribution = .fillEqually
-        thirdColumnVerticalStackView.spacing = 5
-
-        let fourthColumnVerticalStackView = UIStackView(arrangedSubviews: [button[12], button[13], button[14], button[15]])
-        fourthColumnVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        fourthColumnVerticalStackView.axis = .vertical
-        fourthColumnVerticalStackView.alignment = .fill
-        fourthColumnVerticalStackView.distribution = .fillEqually
-        fourthColumnVerticalStackView.spacing = 5
-
-        let fifthColumnVerticalStackView = UIStackView(arrangedSubviews: [button[16], button[17], button[18], button[19]])
-        fifthColumnVerticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        fifthColumnVerticalStackView.axis = .vertical
-        fifthColumnVerticalStackView.alignment = .fill
-        fifthColumnVerticalStackView.distribution = .fillEqually
-        fifthColumnVerticalStackView.spacing = 5
-
-        mainHorizontalStackView.addArrangedSubview(firstColumnVerticalStackView)
-        mainHorizontalStackView.addArrangedSubview(secondColumnVerticalStackView)
-        mainHorizontalStackView.addArrangedSubview(thirdColumnVerticalStackView)
-        mainHorizontalStackView.addArrangedSubview(fourthColumnVerticalStackView)
-        mainHorizontalStackView.addArrangedSubview(fifthColumnVerticalStackView)
-    }
-
     func dataUpdated(models: [[ButtonSymbol]]) {
         var columnStackViews = [UIStackView]()
 
