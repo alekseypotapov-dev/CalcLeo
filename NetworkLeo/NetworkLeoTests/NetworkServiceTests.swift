94e5dc12fcf799280a0e9b81a5d9a344a09ad7eb
Aleksey Potapov <ing.aleksey.potapov@gmail.com>
2021-08-10 22:13:49 +0200

    Logic: replace projects with packages

--- NetworkLeo/NetworkLeoTests/NetworkServiceTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import NetworkLeo
-
-final class NetworkServiceTests: XCTestCase {
-
-    func test_networkService_isAvailable() {
-        XCTAssertEqual(true, NetworkService.isAvailable, "network availability")
-    }
-}
